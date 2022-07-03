f6602cc5403da1ee99623a3c588ebe6abd38d02e
connor <connorskorburg@gmail.com>
2022-07-03 15:20:44 -0500

    add negative number support

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import { useState } from "react";
+import { SetStateAction, Dispatch, useState } from "react";
 import { cells, numbericKeys, operatorKeys } from "./helpers";
 
-const Total = ({ total }: { total: any }): JSX.Element => {
+const Total = ({ total }: { total: string }): JSX.Element => {
   return <div className="total">{total}</div>;
 };
 
@@ -9,53 +9,73 @@ const App = () => {
   const [display, setDisplay] = useState<string>("0");
   const [operator, setOperator] = useState<OperatorType | null>(null);
 
-  const [firstNumber, setFirstNumber] = useState<number>(0);
-  const [secondNumber, setSecondNumber] = useState<number>(0);
+  const [total, setTotal] = useState<number>(0);
+  const [input, setInput] = useState<number>(0);
 
   const clearValues = () => {
     setDisplay("0");
-    setFirstNumber(0);
-    setSecondNumber(0);
+    setTotal(0);
+    setInput(0);
     setOperator(null);
+  };
+
+  const handleNegativeNumber = (
+    value: number,
+    setValue: Dispatch<SetStateAction<number>>
+  ) => {
+    const newValue = value * -1;
+    setValue(newValue);
+    setDisplay(newValue.toString());
   };
 
   const calculateNewValue = () => {
     let newDisplay = 0;
     switch (operator) {
       case "+":
-        newDisplay = firstNumber + secondNumber;
+        newDisplay = total + input;
         break;
       case "x":
-        newDisplay = firstNumber * secondNumber;
+        newDisplay = total * input;
         break;
       case "/":
-        newDisplay = firstNumber / secondNumber;
+        newDisplay = total / input;
         break;
       case "-":
-        newDisplay = firstNumber - secondNumber;
+        newDisplay = total - input;
         break;
     }
     setDisplay(newDisplay.toString());
-    setFirstNumber(newDisplay);
-    setSecondNumber(0);
+    setTotal(newDisplay);
+    setInput(0);
+  };
+
+  const handleKeyChange = (
+    key: KeyBoardType,
+    value: number,
+    setValue: Dispatch<SetStateAction<number>>
+  ) => {
+    if (key === "." && display.includes(".")) return;
+    const newValue = value === 0 ? key : display + key;
+    setValue(parseFloat(newValue));
+    setDisplay(newValue.toString());
   };
 
   const handleButtonClick = (key: OperatorType | KeyBoardType) => {
     if (numbericKeys.includes(key)) {
-      // if (key === "." && display.includes(".")) return;
-      if (!operator) {
-        const first = firstNumber === 0 ? key : display + key;
-        setFirstNumber(parseFloat(first));
-        setDisplay(first.toString());
-      } else {
-        const second = secondNumber === 0 ? key : display + key;
-        setSecondNumber(parseFloat(second));
-        setDisplay(second.toString());
-      }
-    } else if (key === "AC") clearValues();
-    else if (operatorKeys.includes(key)) {
-      if (operator && firstNumber !== 0 && secondNumber !== 0)
-        calculateNewValue();
+      handleKeyChange(
+        key as KeyBoardType,
+        !operator ? total : input,
+        !operator ? setTotal : setInput
+      );
+    } else if (key === "AC") {
+      clearValues();
+    } else if (key === "+/-") {
+      handleNegativeNumber(
+        !operator ? total : input,
+        !operator ? setTotal : setInput
+      );
+    } else if (operatorKeys.includes(key)) {
+      if (operator && total !== 0 && input !== 0) calculateNewValue();
       setOperator(key as OperatorType);
     }
   };
