d807a032af52d1cfa8807370707da6a76e3557c7
connor <connorskorburg@gmail.com>
2022-07-03 21:14:11 -0500

    update tests

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -3,22 +3,11 @@ import App from "../App";
 
 test("adds 4 and 5 together", () => {
   render(<App />);
-  const totalElem = screen.getByTestId("total");
-  const fourElement = screen.getByText("4");
-  const fiveElement = screen.getByText("5");
-  const addElement = screen.getByText("+");
-  const equalElement = screen.getByText("=");
-
-  expect(fourElement).toBeInTheDocument();
-  expect(fiveElement).toBeInTheDocument();
-  expect(addElement).toBeInTheDocument();
-  expect(equalElement).toBeInTheDocument();
-
   // add 4 plus 5 then press equals
-  fireEvent.click(fourElement);
-  fireEvent.click(addElement);
-  fireEvent.click(fiveElement);
-  fireEvent.click(equalElement);
+  fireEvent.click(screen.getByText("4"));
+  fireEvent.click(screen.getByText("+"));
+  fireEvent.click(screen.getByText("5"));
+  fireEvent.click(screen.getByText("="));
 
-  expect(totalElem.innerHTML).toBe("9");
+  expect(screen.getByTestId("total").innerHTML).toBe("9");
 });
