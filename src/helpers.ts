45023fd659a02384cdd648b83a08759db026dca9
connor <connorskorburg@gmail.com>
2022-07-03 19:33:08 -0500

    update operators

--- src/helpers.ts
+++ src/helpers.ts
@@ -80,4 +80,4 @@ export const cells = [
 
 
   export const numbericKeys = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."];
-  export const operatorKeys = ["+", "-", "/", "AC", "%", "+/-", "=", "x"];
+  export const operatorKeys = ["+", "-", "/", "=", "x"];
